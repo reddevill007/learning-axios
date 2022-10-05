0d59cea469a9cd8b7e38644ff7f967411d8e8db8
reddevill007 <shivampandey2900@gmail.com>
2022-10-05 08:16:18 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "./axios";
 
 function App() {
+  const [myData, setMyData] = useState([]);
+  const [error, setError] = useState("");
+
+  const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+  // Axios using promises
+  // useEffect(() => {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/posts")
+  //     .then((res) => setMyData(res.data))
+  //     .catch((err) => setError(err.message));
+  // }, []);
+
+  // Axios using Async Await
+  const getApiData = async () => {
+    try {
+      const res = await axios.get("/posts");
+      setMyData(res.data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getApiData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Axios</h1>
+      {error !== "" && <p>{error}</p>}
+      <div className="flex">
+        {myData.slice(0, 12).map((post) => {
+          const { id, title, body } = post;
+          return (
+            <div className="card" key={id}>
+              <h2>{title.slice(0, 15).toUpperCase()}</h2>
+              <p>{body.slice(0, 100)}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
